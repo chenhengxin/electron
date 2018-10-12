c245904a7bd2bca8831c3fcff217ac9900ed17c1
chx <513889721@qq.com>
2018-10-12 18:29:58 +0800

    2

--- main.js
+++ main.js
@@ -48,3 +48,7 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const ipc = require('electron').ipcMain;
+ipc.on('asynchronous-message', function (event, arg) {
+  event.sender.send('asynchronous-reply', 'pong')
+})
